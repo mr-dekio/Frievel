1e89fa1f7cfd3601066632bf4df77d1300bc80cf
mr-dekio <mr.dekio@yandex.ua>
2015-11-16 20:23:17 +0200

    Added user details view controller

--- Frievel/UserDetailsViewController.swift
+++ Frievel/UserDetailsViewController.swift
@@ -6,8 +6,34 @@
 //  Copyright © 2015 Maxim Galayko. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class UserDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var numberOfFriendsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupAppearance()
+    }
+    
+    // MARK: - Appearance
+    
+    func setupAppearance() {
+        view.backgroundColor = UIColor(patternImage: UIImage(named: backgroundImageName)!)
+        
+        userImageView.layer.cornerRadius = userImageView.bounds.width / 2
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
 
 }
